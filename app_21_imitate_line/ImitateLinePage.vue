1463a4b1b54fff6573745c3aac417591f727b140
cchien <norris@cctech.tw>
2026-07-11 20:50:22 +0800

    微調版型

--- app_21_imitate_line/ImitateLinePage.vue
+++ app_21_imitate_line/ImitateLinePage.vue
@@ -12,8 +12,8 @@
   const cellHeight = ref(96) // 每格高 (px)
   const showGrid = ref(false) // 是否顯示 cell 框線，預設關閉
   const showLines = ref(true) // 是否顯示相鄰點連線，預設開啟
-  const showDiagonals = ref(false) // 是否顯示斜角連線，預設關閉
-  const showOtherLines = ref(false) // 是否顯示其餘連線，預設關閉
+  const showDiagonals = ref(true) // 是否顯示斜角連線，預設關閉
+  const showOtherLines = ref(true) // 是否顯示其餘連線，預設關閉
   const dotRadius = ref(9) // 黃點半徑 (px)，跟 cell 尺寸分開設定
 </script>
 
